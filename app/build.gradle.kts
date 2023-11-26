651398014cb01d567b7447ba88a019fed1f7bd88
Brenton Fletcher <i@bloople.net>
2023-11-27 00:10:11 +1100

    Bump version

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -11,7 +11,7 @@ android {
         applicationId = "net.bloople.appbrowser"
         minSdk = 33
         targetSdk = 33
-        versionCode = 3
+        versionCode = 4
         versionName = "1.0"
     }
 
